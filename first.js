9125c291eb0fcbcf1cf904d18f3fe0423cc6480b
ak15 <157221199+adityasuryawanshi7@users.noreply.github.com>
2024-02-10 17:59:01 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 document.getElementById("holdon").addEventListener("dblclick",function(){
-    window.open("https://adityasuryawanshi7.github.io/heart/")
+    window.open("https://adityasuryawanshi7.github.io/password/")
 }
 
 )
